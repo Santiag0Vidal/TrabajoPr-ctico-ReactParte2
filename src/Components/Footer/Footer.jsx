53750ebd57746e5d70bb58fd972b00509bc3dc99
melaniquininires <melaniquininires@gmail.com>
2024-04-28 16:57:02 -0300

    Cambie el footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import styles from './Footer.module.css'; // Importa el archivo CSS
-
+//import styles from './Footer.module.css'; // Importa el archivo CSS
+//implemento tailwind aca sin usar el Footer.module.css
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
-        <div className={`${styles.flexContainer} justify-between`}>
-            <a href="https://github.com/Santiag0Vidal/trabajo_practico_react_parte2" target="_blank" className={`${styles.link} mx-4`}>GitHub</a>
-            <p className={styles.text}>Grupo x2</p>
-            <p className={styles.text}>Programación Web Avanzada 2024</p>
-        </div>
-    </footer>
+        <footer class="flex flex-col items-center bg-[#1f2937] text-center text-white">
+  <div class="container p-6">
+    <div class="">
+      <p class="flex items-center justify-center">
+        <span class="me-4">Grupo: X2</span>
+       
+      </p>
+    </div>
+  </div>
+
+  <div class="w-full bg-black/20 p-4 text-center">
+    © 2024 Copyright:
+    <a href="https://tw-elements.com/"> Programacion Web Avanzada</a>
+  </div>
+</footer>
     
 
     );
