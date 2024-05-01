367c7007a94ac5c819b1ceec1409fa643dd25da4
Santiag0Vidal <david.vidal@est.fi.uncoma.edu.ar>
2024-05-01 18:31:46 -0300

    agrege los estilos de del Footer dentro de su carpeta modules

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,25 +1,24 @@
+// Footer.jsx
+
 import React from 'react';
-//import styles from './Footer.module.css'; // Importa el archivo CSS
-//implemento tailwind aca sin usar el Footer.module.css
+import styles from './Footer.module.css'; // Importa el archivo CSS
+
 const Footer = () => {
     return (
-        <footer class="flex flex-col items-center bg-[#1f2937] text-center text-white">
-  <div class="container p-6">
-    <div class="">
-      <p class="flex items-center justify-center">
-        <span class="me-4">Grupo: X2</span>
-       
-      </p>
-    </div>
-  </div>
+        <footer className={styles.footerContainer}>
+            <div className={styles.container}>
+                <div className="">
+                    <p className="flex items-center justify-center">
+                        <span className={styles.groupName}>Grupo: X2</span>
+                    </p>
+                </div>
+            </div>
 
-  <div class="w-full bg-black/20 p-4 text-center">
-    © 2024 Copyright:
-    <a href="https://github.com/Santiag0Vidal/trabajo_practico_react_parte2" target="_blank"> Programacion Web Avanzada</a>
-  </div>
-</footer>
-    
-
+            <div className={styles.copyright}>
+                © 2024 Copyright:
+                <a href="https://github.com/Santiag0Vidal/trabajo_practico_react_parte2" target="_blank" rel="noopener noreferrer"> Programación Web Avanzada</a>
+            </div>
+        </footer>
     );
 };
 
